a069f4852453a90f7c3720695fefad78d030318b
Artem Kurchenko <artem.kurchenko@devexpress.com>
2025-09-22 11:10:47 +0400

    small fixes

--- ASP.NET Core/Models/ClientChatMessage.cs
+++ ASP.NET Core/Models/ClientChatMessage.cs
@@ -4,14 +4,12 @@ namespace ASP_NET_Core.Models
 {
     public class ClientChatMessage
     {
-        // For incoming requests (ClientMessage properties)
         public string? timestamp { get; set; }
         
         // Common properties
         public string text { get; set; }
         public string id { get; set; }
         
-        // For outgoing responses (ChatMessageResponse properties)  
         public ChatAuthor? author { get; set; }
     }
 
