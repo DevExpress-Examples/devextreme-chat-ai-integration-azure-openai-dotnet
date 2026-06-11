8189c5f82e53f1dcfcb45f92e33e54a8f07d022f
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-11 20:15:32 +0300

    Remove obsolete eslint-disable directives
    (cherry picked from commit e35c4f0b2e793ab461d8b771b8bc8381df90d237)

--- React/src/components/ChatApp.tsx
+++ React/src/components/ChatApp.tsx
@@ -27,7 +27,6 @@ export default function ChatApp(): JSX.Element {
     appService.onMessageEntered(e, setDisabled);
   }, []);
 
-  /* eslint-disable space-before-function-paren */
   const onRegenerateButtonClick = useCallback(async (): Promise<void> => {
     setDisabled(true);
     appService.updateLastMessage();
